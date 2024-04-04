eff3473b707b359603a0499a72328c4bb83a5e34
lemonface88 <harrison8818@gmail.com>
2024-04-03 23:03:01 -0400

    download data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,17 +11,48 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-
+library(arrow)
 
 #### Download data ####
 
-politics_datasets <-
-  fromJSON("https://demo.dataverse.org/api/search?q=politics")
+
+# All players data
+all_player_datasets <-
+  fromJSON("https://api.sportsdata.io/v3/nba/scores/json/Players?key=92a4662dc81b4f56bd99cc606f483004")
+
+
+#Zion Williamson's data set
+Zion_datasets <-
+  fromJSON("https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2023/20002271/all?key=92a4662dc81b4f56bd99cc606f483004")
+
+#Anthony Edwards's data set
+Anthony_datasets <-
+  fromJSON("https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2023/20002523/all?key=92a4662dc81b4f56bd99cc606f483004")
+
+#Lamelo Ball data set
+LaMelo_datasets <-
+  fromJSON("https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2023/20002528/all?key=92a4662dc81b4f56bd99cc606f483004")
+
+#Tyrese Haliburton
+Haliburton_datasets <-
+  fromJSON("https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2023/20002537/all?key=92a4662dc81b4f56bd99cc606f483004")
+
+#Tyrese Maxey
+maxey_datasets <-
+  fromJSON("https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2023/20002546/all?key=92a4662dc81b4f56bd99cc606f483004")
+
 
 
 #### Save data ####
 
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
+write_csv(all_player_datasets, "data/raw_data/raw_all_player_datasets.csv")
+
+write_csv(Zion_datasets, "data/raw_data/raw_zion_datasets.csv")
+
+write_csv(LaMelo_datasets, "data/raw_data/raw_lamelo_datasets.csv")
+
+write_csv(Haliburton_datasets, "data/raw_data/raw_haliburton_datasets.csv")
+
+write_csv(maxey_datasets, "data/raw_data/raw_maxey_datasets.csv")
          
