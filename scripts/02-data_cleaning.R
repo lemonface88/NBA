475e9ac5f19aaadb2ea40452ce69225f89c54fcb
lemonface88 <harrison8818@gmail.com>
2024-04-04 11:37:15 -0400

    cleaned data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -6,33 +6,54 @@
 # License: MIT
 
 
-
 #### Workspace setup ####
 library(tidyverse)
 library(janitor)
 library(tidyr)
+library(arrow)
 
 #### Clean data ####
 
+#  janitor::clean_names()|>
+#  tidyr::drop_na()|>
 
 # clean zion data
 raw_data_zion <- read_csv("data/raw_data/raw_zion_datasets.csv")
 
 clean_zion_data <-
   raw_data_zion |>
-  janitor::clean_names()|>
-  tidyr::drop_na()|>
+  select(Day, FieldGoalsMade, FieldGoalsAttempted,FieldGoalsPercentage, TwoPointersPercentage, ThreePointersPercentage, FreeThrowsPercentage, Minutes)|>
+  filter(Minutes > 0)
   
-  
 
-
-
+# clean haliburton data
 raw_data_haliburton <- read_csv("data/raw_data/raw_haliburton_datasets.csv")
 
+clean_haliburton_data <-
+  raw_data_haliburton |>
+  select(Day, FieldGoalsMade, FieldGoalsAttempted,FieldGoalsPercentage, TwoPointersPercentage, ThreePointersPercentage, FreeThrowsPercentage, Minutes)|>
+  filter(Minutes > 0)
+
+# clean lamelo data
 raw_data_lamelo <- read_csv("data/raw_data/raw_lamelo_datasets.csv")
 
+clean_lamelo_data <-
+  raw_data_lamelo |>
+  select(Day, FieldGoalsMade, FieldGoalsAttempted,FieldGoalsPercentage, TwoPointersPercentage, ThreePointersPercentage, FreeThrowsPercentage, Minutes)|>
+  filter(Minutes > 0)
+
+
+# clean maxey data
 raw_data_maxey <- read_csv("data/raw_data/raw_maxey_datasets.csv")
+
+clean_maxey_data <-
+  raw_data_maxey |>
+  select(Day, FieldGoalsMade, FieldGoalsAttempted,FieldGoalsPercentage, TwoPointersPercentage, ThreePointersPercentage, FreeThrowsPercentage, Minutes)|>
+  filter(Minutes > 0)
 
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(clean_zion_data, "data/analysis_data/clean_zion_data.parquet")
+
+
+
